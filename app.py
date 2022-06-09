d162e52d4ac251cf6f5e9cb05d49f1cc55d41e4f
Luciano Citroni Lopes <luciano.citroni227@gmail.com>
2022-06-09 15:45:55 -0300

    teste

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ app = Flask(__name__)
 
 @app.route("/<ra>", methods=['GET'])
 def index(ra):
-    if ConsultarRA.consultarRa(ra) == True:
-        return "Encontrou"
-    else:
-        return "Nada foi encontrado"
+    return ConsultarRA.consultarRa(ra)
+    # if ConsultarRA.consultarRa(ra) == True:
+    #     return "Encontrou"
+    # else:
+    #     return "Nada foi encontrado"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
