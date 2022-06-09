bac679fd110a21a718e1fe64166fd8c8a5787546
Luciano Citroni Lopes <luciano.citroni227@gmail.com>
2022-06-09 13:36:01 -0300

    primeiro commit

---
+++ Scripts/ConsultarRA.py
@@ -0,0 +1,10 @@
+import requests
+link = 'https://flutterepy-default-rtdb.firebaseio.com/'
+
+def consultarRa(vRa):
+    consultar = requests.get(f'{link}/Alunos/.json')
+    resultadoConsulta = consultar.json()
+    for idAluno in resultadoConsulta:
+        raResultado = resultadoConsulta[idAluno]['RA']
+        if vRa == raResultado:
+            return True
---
+++ app.py
@@ -0,0 +1,17 @@
+import requests
+from flask import Flask
+import json
+from Scripts import ConsultarRA
+
+app = Flask(__name__)
+
+@app.route('/')
+def ConsultarRa():
+    return 'Olá mundo'
+
+app.run()
+
+if __name__ == '__main__':
+    app.secret_key = 'ItIsASecret'
+    app.debug = True
+    app.run()
